e7526e6ef533ca3f3d5451e173b8d5900d3c0f02
Shreya Agarwal <ashreyaagarwal111@gmail.com>
2021-06-24 23:41:09 +0530

    Add files via upload

---
+++ Deadlock.java
@@ -0,0 +1,49 @@
+public class Deadlock {
+    public static void main(String[] args) {
+        final String r1 = "shreya";
+        final String r2 = "agarwal";
+        Thread t1 = new Thread(){
+        public void run ()
+        {
+            synchronized (r1) {
+                System.out.println("Thread 1: locked resource 1");
+
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+
+                }
+
+                synchronized (r2) {
+                    System.out.println("Thread 1: locked resource 2");
+                }
+            }
+            }
+        };
+Thread t2 = new Thread()
+{
+    public void run()
+    {
+        synchronized (r2)
+        {
+            System.out.println("Thread 2: locked resource 2");
+
+            try {
+                Thread.sleep(100);
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            synchronized (r1)
+            {
+                System.out.println("Thread 2: locked resource 1");
+            }
+        }
+    }
+};
+t1.start();
+t2.start();
+}
+}
