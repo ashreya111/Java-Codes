476a64a873e2ecbf9702ef574c04cde6f9b934a6
Shreya Agarwal <ashreyaagarwal111@gmail.com>
2021-06-25 15:56:30 +0530

    Add files via upload

---
+++ PerfectNumberOrNot.java
@@ -0,0 +1,31 @@
+//Write a program to find perfect number or not.
+
+import java.util.Scanner;
+
+public class PerfectNumberOrNot {
+    static boolean isPerfect(int n)
+    {
+        if(n==1)
+            return false;
+        int sum = 1;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+                sum+=i;
+        }
+        if(sum==n)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (isPerfect(n))
+            System.out.println(n + " is a perfect number");
+        else
+            System.out.println(
+                    n + " is not a perfect number");
+    }
+}
